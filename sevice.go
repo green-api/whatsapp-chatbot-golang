3b2881df1435da84390263a959b1d591303a93cd
dany47788 <65661948+dany47788@users.noreply.github.com>
2024-02-09 18:48:10 -0300

    add methods without quote

--- sevice.go
+++ sevice.go
@@ -108,3 +108,100 @@ func (n *Notification) AnswerWithContact(contact map[string]interface{}) map[str
 
 	return response
 }
+
+func (n *Notification) SendText(text string) map[string]interface{} {
+	chatId := n.Body["senderData"].(map[string]interface{})["chatId"].(string)
+	response, err := n.GreenAPI.Methods().Sending().SendMessage(map[string]interface{}{
+		"chatId":  chatId,
+		"message": text,
+	})
+
+	if err != nil {
+		*n.ErrorChannel <- err
+		return map[string]interface{}{"error": err}
+	}
+
+	return response
+}
+
+func (n *Notification) SendUploadFile(filePath string, caption string) map[string]interface{} {
+	chatId := n.Body["senderData"].(map[string]interface{})["chatId"].(string)
+	response, err := n.GreenAPI.Methods().Sending().SendFileByUpload(filePath, map[string]interface{}{
+		"chatId":  chatId,
+		"caption": caption,
+	})
+
+	if err != nil {
+		*n.ErrorChannel <- err
+		return map[string]interface{}{"error": err}
+	}
+
+	return response
+}
+
+func (n *Notification) SendUrlFile(urlFile string, filename string, caption string) map[string]interface{} {
+	chatId := n.Body["senderData"].(map[string]interface{})["chatId"].(string)
+	response, err := n.GreenAPI.Methods().Sending().SendFileByUrl(map[string]interface{}{
+		"chatId":   chatId,
+		"urlFile":  urlFile,
+		"fileName": filename,
+		"caption":  caption,
+	})
+
+	if err != nil {
+		*n.ErrorChannel <- err
+		return map[string]interface{}{"error": err}
+	}
+
+	return response
+}
+
+func (n *Notification) SendLocation(nameLocation string, address string, latitude float64, longitude float64) map[string]interface{} {
+	chatId := n.Body["senderData"].(map[string]interface{})["chatId"].(string)
+	response, err := n.GreenAPI.Methods().Sending().SendLocation(map[string]interface{}{
+		"chatId":       chatId,
+		"nameLocation": nameLocation,
+		"address":      address,
+		"latitude":     latitude,
+		"longitude":    longitude,
+	})
+
+	if err != nil {
+		*n.ErrorChannel <- err
+		return map[string]interface{}{"error": err}
+	}
+
+	return response
+}
+
+func (n *Notification) SendPoll(message string, multipleAnswers bool, options []map[string]interface{}) map[string]interface{} {
+	chatId := n.Body["senderData"].(map[string]interface{})["chatId"].(string)
+	response, err := n.GreenAPI.Methods().Sending().SendPoll(map[string]interface{}{
+		"chatId":          chatId,
+		"message":         message,
+		"options":         options,
+		"multipleAnswers": multipleAnswers,
+	})
+
+	if err != nil {
+		*n.ErrorChannel <- err
+		return map[string]interface{}{"error": err}
+	}
+
+	return response
+}
+
+func (n *Notification) SendContact(contact map[string]interface{}) map[string]interface{} {
+	chatId := n.Body["senderData"].(map[string]interface{})["chatId"].(string)
+	response, err := n.GreenAPI.Methods().Sending().SendContact(map[string]interface{}{
+		"chatId":  chatId,
+		"contact": contact,
+	})
+
+	if err != nil {
+		*n.ErrorChannel <- err
+		return map[string]interface{}{"error": err}
+	}
+
+	return response
+}
